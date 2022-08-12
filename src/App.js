53a2ddbae95b94c42fc179e393847722fc92ae31
Denasia <dfurniss@ymail.com>
2022-08-11 23:11:30 -0400

    wiped Mahjong, starting a new layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import "./App.css";
 import Header from "./Header";
-import MahjongContainer from "./MahjongContainer";
 
 function App() {
   return (
   <>
       <Header/>
     <div className="container-fluid px-4">
-      <MahjongContainer/>
     </div>
   </>
   );
--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,5 @@
 export default function Header(){
     const styles = "container-fluid text-center py-4 bg-warning text-light"
 
-    return <h1 className={styles}>World of Mahjong</h1>
+    return <h1 className={styles}>Filtering</h1>
 }
