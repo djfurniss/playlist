24f38f99aa326dcd8903be3fcd47828a29ea7fc1
Denasia <dfurniss@ymail.com>
2022-11-09 09:19:01 -0500

    commented out the use of the spotify API

--- client/src/Home/Home.js
+++ client/src/Home/Home.js
@@ -1,11 +1,77 @@
-import React from "react";
-import { useNavigate } from "react-router-dom"
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from 'axios';
 
 function Home () {
+
+const CLIENT_ID = "fc77e26dd7e54da399a17ce5607772e1"
+const REDIRECT_URI = "http://localhost:3000"
+const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+const RESPONSE_TYPE = "token"
+
     const navigate = useNavigate();
+    // const [token, setToken] = useState("")
+    // const [searchKey, setSearchKey] = useState("")
+    // const [songs, setSongs] = useState([])
+
+    // useEffect(() => {
+    //     const hash = window.location.hash
+    //     let token = window.localStorage.getItem("token")
+    
+    //     if (!token && hash) {
+    //         token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+    
+    //         window.location.hash = ""
+    //         window.localStorage.setItem("token", token)
+    //     }
+    
+    //     setToken(token)
+    
+    // }, [])
+
+    // const logout = () => {
+    //     setToken("")
+    //     window.localStorage.removeItem("token")
+    // };
+
+    // const searchArtists = async (e) => {
+    //     e.preventDefault()
+    //     const {data} = await axios.get("https://api.spotify.com/v1/search/", {
+    //         headers: {
+    //             Authorization: `Bearer ${token}`
+    //         },
+    //         params: {
+    //             q: searchKey,
+    //             type: "track",
+    //             "include_external": "audio",
+    //         }
+    //     })
+    //     console.log(data.tracks.items)
+    //     setSongs(data.tracks.items)
+    // }
 
     return (
         <main>
+            {/* <div className="App">
+                <header className="App-header">
+                    <h1>Spotify React</h1>
+                    {!token ?
+                        <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login
+                            to Spotify</a>
+                        : <button onClick={logout}>Logout</button>}
+                </header>
+            </div>
+
+            <form onSubmit={searchArtists}>
+                <input type="text" onChange={({ target }) => setSearchKey(target.value)}/>
+                <button type={"submit"}>Search</button>
+            </form>
+
+           {songs.length && songs.map(song => (
+                <div key={song.id}>
+                    <p>{song.name} by {song.artists[0].name}</p>
+                </div>
+            ))} */}
             <h1 className="text-center">Welcome to GrouPlaylist</h1>
             <div className="text-center">
                 <p 
