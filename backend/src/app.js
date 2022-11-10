4824690c1008ec52032734c4b76a9fe75bc24c07
Denasia <dfurniss@ymail.com>
2022-11-10 18:38:34 -0500

    moved functions into playlist resource directory

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const router = require("./router");
 const morgan = require("morgan");
+
 const pageNotFound = require("./errors/pageNotFound")
 const errorHandle = require("./errors/errorHandle")
+
+const playlistsRouter = require("./playlists/playlists.router");
+const usersRouter = require("./users/users.router")
 
 app.use(morgan("dev"))
 app.use(express.json())
 app.use(cors())
 
-app.use("/", router)
+app.use("/playlists", playlistsRouter)
+// app.use("/users", usersRouter)
 
 app.use(errorHandle)
 app.use(pageNotFound)
