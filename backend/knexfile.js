00c44410e18d5e349e17a075b5daadd6dc40831e
Denasia <dfurniss@ymail.com>
2022-12-10 22:25:55 -0600

    added test database connection

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -6,6 +6,7 @@ require("dotenv").config();
 
 const { 
   DATABASE_URL, 
+  TEST_DATABASE_URL,
   PRODUCTION_DATABASE_URL,
 } = process.env
 
@@ -13,6 +14,16 @@ module.exports = {
   development: {
     client: 'pg',
     connection: DATABASE_URL,
+    migrations: {
+      directory: "src/db/migrations"
+    },
+    seeds: {
+      directory: "src/db/seeds"
+    }
+  },
+  test: {
+    client: 'pg',
+    connection: TEST_DATABASE_URL,
     migrations: {
       directory: "src/db/migrations"
     },
