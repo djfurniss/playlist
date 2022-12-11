722a6b96ae7bc7876ccd6495a0bae55dc2423907
Denasia <dfurniss@ymail.com>
2022-12-10 22:27:28 -0600

    seeded users data with hash instead of plain password text

--- backend/src/db/fixtures/users.js
+++ backend/src/db/fixtures/users.js
@@ -2,19 +2,19 @@ module.exports = [
     {
         "user_id": 1,
         "user_name": "djf",
-        "password": "jaliece2010",
+        "password": "cf00e7c3b773ebd72c681fabdb81c979ca6ece9575e39c583a5a3ae275ddcd87",
         "email": "dfurniss@ymail.com",
     },
     {
         "user_id": 2,
         "user_name": "dfurniss",
-        "password": "jaliece2010",
+        "password": "cf00e7c3b773ebd72c681fabdb81c979ca6ece9575e39c583a5a3ae275ddcd87",
         "email": "denasiafurniss@gmail.com",
     },
     {
         "user_id": 3,
         "user_name": "bobby412",
-        "password": "password",
+        "password": "742d7b38a9ffd280abe7d115d76268478d2b8c736ff72945c8b4e0bc982a46ba",
         "email": "jalieve2010@gmail.com",
     },
 ]
